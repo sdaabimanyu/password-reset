bcd8bdc1fc2f740e4b2a3c7f5260cdefbcd56154
sdaabimanyu <sdaabimanyu@gmail.com>
2026-04-29 18:27:44 +0530

    clean commit without env

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,19 +1,32 @@
-const nodemailer = require("nodemailer");
+const axios = require("axios");
 
-async function sendEmail(email, link) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  await transporter.sendMail({
-    from: process.env.EMAIL,
-    to: email,
-    subject: "Password Reset",
-    html: `<p>Click here to reset passowrd:<a href="${link}">${link}</a></p>`,
-  });
-}
+const sendEmail = async (to, link) => {
+  try {
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Password Reset",
+          email: process.env.EMAIL_USER,
+        },
+        to: [{ email: to }],
+        subject: "Reset Your Password",
+        htmlContent: `
+          <h3>Password Reset</h3>
+          <p>Click below link to reset your password:</p>
+          <a href="${link}">${link}</a>
+        `,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+  }
+};
 
 module.exports = sendEmail;
